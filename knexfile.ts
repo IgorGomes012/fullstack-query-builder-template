df9667eb4e31f26f5233a7fb3f61bea12a7a91eb
igor gomes <gomesoliveirasl02@hotmail.com>
2025-03-09 12:58:42 -0300

    Novo

--- knexfile.ts
+++ knexfile.ts
@@ -6,7 +6,7 @@ export default {
   useNullAsDefault: true,
   migrations: {
     extensions: "ts",
-    directory: "./src/database/migrations",
+    directory: "./src/database/migrationsaa",
 
   }
 };
